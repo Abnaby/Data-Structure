0e9aa68aa4d478697a1fe72546c9274b6709fbb3
Abnaby <mahameda.naby@gmail.com>
2021-07-27 16:13:29 +0200

    Linked STACK V1

---
+++ Stack/Dynamic Array/linkedStack.cpp
@@ -0,0 +1,86 @@
+/**************************************************************************/
+/* Author	: Mohamed                                               	  */
+/* Date		: 27 July 2021                                 		    	 */
+/* Version	: V01							  							  */
+/**************************************************************************/
+#include <string>
+template <class workingDataType>
+class stack
+{
+
+public:
+	stack() {
+		top = NULL;
+		numberOfNodes = 0;
+	}
+	~stack()
+	{
+		delete top;
+	}
+	void pushToStack(workingDataType copy_PushItemToStack)
+	{
+		// Create malloc of struct size 
+		Node* newItemPtr = new Node;
+		//Check process for allocating mem 
+		if(newItemPtr != NULL) 
+		{ 
+			numberOfNodes++;
+			//Add Vlaue to stack
+			newItemPtr->nodeNumValue = copy_PushItemToStack;
+			//Link New Node With Old Node 
+			newItemPtr->nextPointer = top;
+			// Change Top/Head to newest Node 
+			top = newItemPtr;
+		}
+		else
+		{
+			// <TODO ERROR> if newItemPtr = NULL cann't allocate memory
+		}
+	}
+	bool isEmpty(void)
+	{
+		return top == NULL; 
+	}
+	void popFromStack(void)
+	{
+		if (!isEmpty())
+		{
+			// make temp ptr point to last node 
+			Node* tempPtr = top; 
+			// Make TopPtr point to new place
+			top = top->nextPointer; 
+			// Disconnect Between two nodes
+			tempPtr = tempPtr->nextPointer = NULL;
+			delete tempPtr; 
+		}
+		else
+		{
+			//<TODO ERROR> stack is empty
+		}
+	}
+	int getNumberOfElement(void)
+	{
+		return numberOfNodes ;
+	}
+	workingDataType getTopOfStack(void)
+	{
+		if (!isEmpty())
+		{
+			return top->nodeNumValue; 
+		}
+		else
+		{
+			//<TODO ERROR> stack is empty
+		}
+	}
+
+	
+private:
+	struct Node
+	{
+		workingDataType nodeNumValue;
+		Node* nextPointer;
+	};
+	Node* top; 
+	int numberOfNodes; 
+};
